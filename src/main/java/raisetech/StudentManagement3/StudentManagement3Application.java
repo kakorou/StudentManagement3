7fb791b7e6077b4253812557923655a278251edf
シブエ <kakorou399@gmail.com>
2024-11-30 19:54:51 +0900

    初回コミット

---
+++ src/main/java/raisetech/StudentManagement3/StudentManagement3Application.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagement3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagement3Application.class, args);
+	}
+
+}
---
+++ src/test/java/raisetech/StudentManagement3/StudentManagement3ApplicationTests.java
@@ -0,0 +1,13 @@
+package raisetech.StudentManagement3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagement3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
